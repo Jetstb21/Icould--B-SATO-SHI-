122258f30c8db2b3836cd4f78ac09cb1ae384655
Jetstb21 <120574261+Jetstb21@users.noreply.github.com>
2025-08-30 15:59:08 -0600

    Update main page to use Comparison component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import BenchmarkRadar from "../components/BenchmarkRadar";
+import Comparison from "@/components/Comparison";
 
 export default function App() {
   return (
-    <main>
-      <BenchmarkRadar />
+    <main className="min-h-screen bg-gray-50">
+      <h1 className="text-3xl font-bold text-center my-6">Could I Be Satoshi?</h1>
+      <Comparison />
     </main>
   );
 }
