87ac8e2cb801fd6eae0a9bc309cfb6816563cfc9
Jetstb21 <120574261+Jetstb21@users.noreply.github.com>
2025-08-30 18:41:51 -0600

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  'https://ochujlrskwybjtxizffp.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jaHVqbHJza3d5Ymp0eGl6ZmZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1NzIxOTcsImV4cCI6MjA3MjE0ODE5N30._rLg4Wmcdsu0TxVJYJuB20bu0oVmPgLdKnyDV52Fglk'
 )
